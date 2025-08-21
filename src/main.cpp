8bef5f31470fdb2fe493fb8ecb7ff2fc105c5b0c
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2025-08-21 15:42:25 +0300

    Добавлена тестовая функция

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,10 @@
 #include <iostream>
 
 #include "myvector/myvector.h"
+#include "containertest.h"
 
 int main() {
     std::cout << "*****************" << std::endl;
 
-    MyVector<int> vec{0, 1, 2, 3, 4};
-    vec.push_back(5);
-    vec.push_back(6);
-    vec.push_back(7);
-    vec.push_back(8);
-    vec.push_back(9);
-    // vec.push_back(10);
-
-    vec.print();
-
-    vec.erase(0);
-    vec.print();
+    containerTest<MyVector<int>>();
 }
